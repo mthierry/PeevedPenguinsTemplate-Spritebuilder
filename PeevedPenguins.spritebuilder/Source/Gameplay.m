db1d8933e6e108759b985414506509f63c3381a9
Michel Thierry <github@michelthierry.org>
2014-11-08 17:32:37 +0000

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -60,7 +60,7 @@
         _currentPenguin.physicsBody.allowsRotation = FALSE;
         
         // create a joint to keep the penguin fixed to the scoop until the catapult is released
-        //_penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
+        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
     }
 }
 
